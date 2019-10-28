49fe8c0d9174f12949410df0889151bd383c3eb0
Quantum299 <firasamous2s4@gmail.com>
2019-10-28 17:20:18 +0100

    git/done

---
+++ project.js
@@ -0,0 +1,47 @@
+/*
+
+
+Question 1
+C - git clone
+
+Question 2
+What git command would you use to see the current state of the project, or to see what is not staged?
+B - git status
+
+Question 3
+Which command would you use to remove all of the '.json' files from the current directory?
+C - rm *.json
+
+Question 4
+Which of these commands will delete a directory named 'config' from your current one.
+B - rm config
+
+
+Question 6
+Which of the following commands allows you to specify the parent repo from which you forked your current local copy.
+B - git remote
+
+Question 7
+How do you rename your "config" folder to "setup" so instead of this
+$ git mv config setup
+B - Using the "mv" command, I move config to setup.
+
+Question 8
+In the terminal, how would you move up one directory
+1 - cd ..
+
+Other Question 8
+From the command-line, how would you move from the "db/" directory to the "public/" directory in a project folder that looks like this?
+
+
+Other
+Answer 8
+2 - cd ../../public
+
+
+
+
+
+
+
+
